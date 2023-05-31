85a964cbb21fdbcff00ca6e213ba26f4081efde9
AndrewPolak <16217753+AndrewPolak@users.noreply.github.com>
2023-06-01 09:56:37 +1000

    Insert test for empty string

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    renderLeads()
+    if (inputEl.value) {
+        myLeads.push(inputEl.value)
+        renderLeads()
+        inputEl.value = ""
+    }
 })
 
 function renderLeads() {
