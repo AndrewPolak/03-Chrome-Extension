ff50c9dcd0948b7bbded6a363c24d5b31f9fb22f
AndrewPolak <16217753+AndrewPolak@users.noreply.github.com>
2023-06-01 10:30:21 +1000

    Add href link to lead list

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads = ["https://www.google.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
@@ -17,7 +17,7 @@ inputBtn.addEventListener("click", function() {
 function renderLeads() {
     let listItems = ""
     for(let i = 0; i < myLeads.length; i++) {
-        listItems += "<li>" + myLeads[i] + "</li>"
+        listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
     }
     ulEl.innerHTML = listItems
 }
