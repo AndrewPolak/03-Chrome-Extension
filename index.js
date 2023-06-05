5a58ed56f7873ba5ed0b2794f8b6855b263e8075
AndrewPolak <16217753+AndrewPolak@users.noreply.github.com>
2023-06-05 10:33:11 +1000

    Add Init Function; Add delete-btn listener;

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 
-let myLeads = ["https://www.google.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const deleteBtn = document.getElementById("delete-btn")
 const ulEl = document.getElementById("ul-el")
 
-renderLeads()
+init()
 
-inputBtn.addEventListener("click", function() {
-    if (inputEl.value) {
-        myLeads.push(inputEl.value)
-        renderLeads()
-        inputEl.value = ""
+function init() {
+    let localLeads = localStorage.getItem("myLeads")
+    if (localLeads === null) {
+        myLeads = []
+    } else {
+        myLeads = JSON.parse(localLeads)
     }
-})
+    renderLeads()
+}
 
 function renderLeads() {
     let listItems = ""
     for(let i = 0; i < myLeads.length; i++) {
-
         listItems += `
             <li>
                 <a target='_blank' href='${myLeads[i]}'>${myLeads[i]}</a>
@@ -27,3 +29,17 @@ function renderLeads() {
     ulEl.innerHTML = listItems
 }
 
+inputBtn.addEventListener("click", function() {
+    if (inputEl.value) {
+        myLeads.push(inputEl.value)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        inputEl.value = ""
+        renderLeads()
+    }
+})
+
+deleteBtn.addEventListener("click", function() {
+    localStorage.removeItem("myLeads")
+    myLeads = []
+    renderLeads()
+})
